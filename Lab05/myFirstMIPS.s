05146c38fa5bf2092b831a23c2620cc286977173
sidkrishna07 <krishk378@gmail.com>
2024-03-05 17:54:35 -0800

    added assembly basics code

---
+++ Lab05/myFirstMIPS.s
@@ -0,0 +1,31 @@
+
+
+    .text
+    .globl main
+
+main:
+    # $t0 = $s0
+    move $t0, $s0
+    
+    # $t1 = $t0 - 7
+    sub $t1, $t0, 7
+    
+    # $t2 = $t1 + $t0
+    add $t2, $t1, $t0
+    
+    # $t3 = $t2 + 2
+    addi $t3, $t2, 2
+    
+    # $t4 = $t3 + $t2
+    add $t4, $t3, $t2
+    
+    # $t5 = $t4 - 28
+    sub $t5, $t4, 28
+    
+    # $t6 = $t4 - $t5
+    sub $t6, $t4, $t5
+    
+    # $t7 = $t6 + 12
+    addi $t7, $t6, 12
+    
+    j main
