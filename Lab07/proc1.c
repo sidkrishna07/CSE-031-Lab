91adfa66c60e8f16c5d1a3363c2953bb41847815
Siddharth Krishna <krishk378@gmail.com>
2024-03-18 18:27:05 -0700

    tps done

---
+++ Lab07/proc1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int m = 10;
+    int n = 5;
+    int result;
+
+    // Equivalent of SUM function in MIPS
+    result = m + n;
+
+    // Equivalent of printing out the result in MIPS
+    printf("%d\n", result);
+
+    return 0;
+}
