d5fbf2e2f3832fc8b4e83649b1944683ce1ccb3d
sidkrishna07 <krishk378@gmail.com>
2024-04-07 23:13:36 -0700

    adjusting comments

--- Lab08/recursion1.s
+++ Lab08/recursion1.s
@@ -37,7 +37,7 @@ main:
 
 # Recursion Function
 recursion:
-    # Base cases: m == -1 or m <= -2
+    # Base cases: m == -1 or m <= -2 
     li $t0, -1
     beq $a0, $t0, base_case_1
     
@@ -45,31 +45,32 @@ recursion:
     ble $a0, $t1, base_case_2
 
     # Recursive case: Perform the recursion with m-3 and m-2, then add results and m
-    addi $sp, $sp, -12
+    addi $sp, $sp, -12   #allocating space on stack for three words
     
     sw $ra, 8($sp)
-    sw $a0, 4($sp)
+    sw $a0, 4($sp) #saving value of m
     sw $t2, 0($sp)
     
     addi $a0, $a0, -3
     
-    jal recursion
+    jal recursion    #recursive call with m - 3
     
-    move $t2, $v0
+    move $t2, $v0      #saving result of m - 3 recursion
     lw $a0, 4($sp)
     addi $a0, $a0, -2
     
-    jal recursion
+    jal recursion       #recursive call with m - 2
     
-    add $v0, $v0, $t2
+    add $v0, $v0, $t2   #adding result of m - 3 and m - 2
     lw $t2, 0($sp)
     lw $a0, 4($sp)
     
-    add $v0, $v0, $a0
+    add $v0, $v0, $a0    # adding m to the result of m - 3 and m - 2
     lw $ra, 8($sp)
     
-    addi $sp, $sp, 12
+    addi $sp, $sp, 12  #freeing 12 bytes of space on stack 
     jr $ra
+
 
 base_case_1:
     li $v0, 3
